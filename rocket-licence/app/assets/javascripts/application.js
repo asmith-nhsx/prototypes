a144fc3f1ed6eb42490fdb59196e0138c6202d95
Alex Smith <88447828+asmith-nhsx@users.noreply.github.com>
2022-11-23 15:53:26 +0000

    Fix the prototype to work with v13 of the kit and also update the accessible autocomplete so it works too

--- rocket-licence/app/assets/javascripts/application.js
+++ rocket-licence/app/assets/javascripts/application.js
@@ -4,34 +4,3 @@
 if (window.console && window.console.info) {
   window.console.info('GOV.UK Prototype Kit - do not use for production')
 }
-
-$(document).ready(function () {
-  window.GOVUKFrontend.initAll()
-
-  let selectElement = document.querySelector('#launch-country')
-  let selectedCountry = document.querySelector('#launch-country-selected').value
-
-  selectElement.value = selectedCountry
-
-  accessibleAutocomplete.enhanceSelectElement({
-    defaultValue: "",
-    source: (query, populateResults) => {
-      const options = selectElement.querySelectorAll('option')
-      let results = []
-
-      options.forEach(
-        (opt, i, list) => {
-          let queryRegExp = new RegExp(query.trim(), 'i')
-          let value = opt.getAttribute('value')
-          let text = opt.innerText.trim()
-          if (queryRegExp.test(value) || queryRegExp.test(text)) {
-            results.push(text)
-          }
-        }
-      )
-
-      populateResults(results)
-    },
-    selectElement: selectElement
-  })
-})
